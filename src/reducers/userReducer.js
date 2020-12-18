880db3e4ae1201a609c993296c01d26b4db77660
joannayhs <joannayuhas@gmail.com>
2020-12-17 22:45:11 -0500

    fixed login method in userreducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,8 @@
 import { SET_CURRENT_USER, CLEAR_CURRENT_USER } from '../actionTypes/index'
 
-
 export function userReducer(state=null, action) {
     switch(action.type){
         case SET_CURRENT_USER:
-            console.log("here")
             return action.user
         case CLEAR_CURRENT_USER:
             return null
