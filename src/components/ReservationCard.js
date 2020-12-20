cc63476e5dbc48bae101b9db9a475dc69047e0c8
joannayhs <joannayuhas@gmail.com>
2020-12-19 22:33:38 -0500

    added reservation properties to res card

--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -3,9 +3,11 @@ export default function ReservationCard(props){
 
     return(
         <div className="ReservationCard">
-            {console.log(props)}
-            <h3>Your Trip to:</h3>
             
+            <h3>Your Trip to:</h3>
+            {props.listing ? <h4>{props.listing.attributes.property.city}</h4> : null }
+            {props.reservation ? <p>{props.reservation.start_date} to {props.reservation.end_date}</p> : null}
+
         </div>
     )
 }
--- src/components/ReservationsContainer.js
+++ src/components/ReservationsContainer.js
@@ -1,29 +1,30 @@
 import ReservationCard from './ReservationCard'
 import { connect } from 'react-redux'
+import { useEffect } from 'react'
 
 function ReservationsContainer(props){
     const userReservations = props.user.attributes.reservations
-
+    
     const resListingId = reservation => {
         for(const res in reservation){
             return reservation[res].listing_id
         }
     }
-
+    
     const getListing = (listings) => {
         for(const l in listings){   
             if (resListingId(userReservations) === Number(listings[l].id)){
-                   return listings[l]
-               }
-           }
-    }
-    
-
-    const renderReservationCards = () => {
-        for(const r in userReservations){  
-            return <ReservationCard reservations={userReservations[r]} listing={getListing(props.listings)} key={userReservations[r].id}/>
+                return listings[l]
+            }
         }
     }
+    
+    const renderReservationCards = () => {
+        for(const r in userReservations){  
+            return <ReservationCard reservation={userReservations[r]} listing={getListing(props.listings)} key={userReservations[r].id}/>
+        }
+    }
+
     return (
         <div className="ReservationsContainer">
             {renderReservationCards()}
