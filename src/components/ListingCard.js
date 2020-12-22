e897782dbc864253485bc325961fb98131245efe
joannayhs <joannayuhas@gmail.com>
2020-12-22 15:30:29 -0500

    added key to image

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -5,7 +5,7 @@ export default function ListingCard({listing}){
     return (
         <div className="ListingCard">
             <h3>{listing.attributes.title}</h3>
-            {listing.attributes.images.map(i => <img src={i.url} height="25%" width="25%"/>)}
+            {listing.attributes.images.map(i => <img src={i.url} key={i.description} height="25%" width="25%"/>)}
             <p>{listing.attributes.description}</p>
             <p>Max Guests: {listing.attributes.max_guests}</p>
             <p>Number of Beds: {listing.attributes.num_of_beds}</p>
