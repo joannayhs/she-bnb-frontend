b89c3fc8431ffffeea4f7ea06ab6369f1bc5f408
joannayhs <joannayuhas@gmail.com>
2020-12-22 15:26:42 -0500

    fixed issue with only one picture rendering

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,32 +1,11 @@
-
-import { Carousel } from 'react-bootstrap'
+import React from 'react'
 
 export default function ListingCard({listing}){
 
-    function renderImgs(){
-        const images = listing.attributes.images
-        for(const i in images){
-            return <img src={images[i].url}/>
-                    // <Carousel.Item>
-                    //     <img
-                    //         className="d-block w-100"
-                    //         src={images[i].url}
-                    //         alt={images[i].description}
-                    //     />
-                    //     <Carousel.Caption>
-                    //         <p>{images[i].description}</p>
-                    //     </Carousel.Caption>
-                    // </Carousel.Item>
-            
-        }
-    }
-
-
-    
     return (
         <div className="ListingCard">
             <h3>{listing.attributes.title}</h3>
-            {renderImgs()}
+            {listing.attributes.images.map(i => <img src={i.url} height="25%" width="25%"/>)}
             <p>{listing.attributes.description}</p>
             <p>Max Guests: {listing.attributes.max_guests}</p>
             <p>Number of Beds: {listing.attributes.num_of_beds}</p>
