6ef754f6f0c8425cde9b5fec71562325bc3ed401
joannayhs <joannayuhas@gmail.com>
2020-12-23 22:13:42 -0500

    added input fields for property

--- src/components/AddListingForm.js
+++ src/components/AddListingForm.js
@@ -2,7 +2,8 @@ import { connect } from 'react-redux'
 import { useState } from 'react'
 
 function AddListingForm({ amenities }){
-const [formData, setFormData] = useState({"Amenities": {}, "Images":{}})
+const [formData, setFormData] = useState({"Amenities": {}, "Images":{}, "Property":{}})
+const stateAbrevs = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MN', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
     function renderAmenityCheckboxes(){
       return Object.keys(amenities).map(a => {
@@ -24,6 +25,8 @@ const [formData, setFormData] = useState({"Amenities": {}, "Images":{}})
             listingData["Images"][e.target.name] = e.target.value
         }if(e.target.name === 'new_amenity'){
             listingData["Amenities"][e.target.name] = { 'name': e.target.value }
+        }if(e.target.name === 'property'){
+            listingData['Property'][e.target.id] = e.target.value
         }else{
             listingData[e.target.name] = e.target.value
         }
@@ -42,6 +45,19 @@ const [formData, setFormData] = useState({"Amenities": {}, "Images":{}})
             <form>
                 Title: <input type="text" placeholder="Title" name="title" onChange={handleOnChange}/><br/>
                 Description: <input type="text" placeholder="Description" name="description" onChange={handleOnChange}/><br/>
+                <br/>
+
+                Address:<br/>
+                Street: <input type="text" name="property" placeholder="Street" id="street" onChange={handleOnChange}/><br/>
+                City: <input type="text" name="property" placeholder="City" id="city" onChange={handleOnChange}/><br/>
+                State: 
+                <select name="property" id="state" onChange={handleOnChange}>
+                    <option> </option>
+                    {stateAbrevs.map(s => <option>{s}</option>)}
+                </select><br/>
+                Zip: <input type="text" name="property" id="zip" onChange={handleOnChange}/><br/>
+                
+                <br/>
                 Type of Place:
                 <select name="type_of" onChange={handleOnChange}>
                     <option name=""></option>
@@ -50,6 +66,7 @@ const [formData, setFormData] = useState({"Amenities": {}, "Images":{}})
                     <option name="Hotel Room">Hotel Room</option>
                     <option name="Shared Room">Shared Room</option>
                 </select><br/>
+
                 Maximum Guests Allowed:<br/>
                 <input type="number" name="max_guests" onChange={handleOnChange}/><br/>
                 Number of Beds Available:<br/>
@@ -69,6 +86,7 @@ const [formData, setFormData] = useState({"Amenities": {}, "Images":{}})
                 Description: <input type="text" name="img_description" onChange={handleOnChange} /><br />
                 <input type="text" placeholder="Image URL" name="img_url" onChange={handleOnChange} /><br />
                 Description: <input type="text" name="img_description" onChange={handleOnChange} /><br />
+                <br/>
                 <button type="submit" onClick={handleSubmit}>Add Listing</button>
             </form>
         </div>
