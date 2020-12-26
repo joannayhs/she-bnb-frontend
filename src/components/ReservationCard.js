06aac68a988fd8189f818bbc78f76c7335d794f2
joannayhs <joannayuhas@gmail.com>
2020-12-26 13:42:23 -0500

    added function to fetch property in reservation card

--- src/components/ReservationCard.js
+++ src/components/ReservationCard.js
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react'
+
 export default function ReservationCard(props){
-    
+    const [ property, setProperty ] = useState('')
+
+     function getProperty(){
+        fetch(`http://localhost:3001/api/v1/properties/${props.listing.relationships.property.data.id}`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(property => {
+            setProperty(property.data)
+        })
+        .catch("Unable to fetch property")
+    }
+
+    useEffect(()=>{
+        getProperty()
+        console.log(property)
+    }, [])
 
     return(
         <div className="ReservationCard">
             
             <h3>Your Trip to:</h3>
-            {props.listing ? <h4>{props.listing.attributes.property.city}</h4> : null }
+            {props.listing ? <h4>{property.attributes.city}</h4> : null }
             {props.reservation ? <p>{props.reservation.start_date} to {props.reservation.end_date}</p> : null}
 
         </div>
