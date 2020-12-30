675b54ec93e0b57c8b2d2b05b3516266a758fc74
joannayhs <joannayuhas@gmail.com>
2020-12-30 16:04:17 -0500

    added property fetch request to listing page

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -4,7 +4,7 @@ export default function ListingCard({listing}){
 
     return (
         <div className="ListingCard">
-            <NavLink to={`listings/${listing.id}`}>{listing.attributes.title}</NavLink>
+            <NavLink to={`listings/${listing.id}`}>{listing.attributes.title}</NavLink><br/>
             {listing.attributes.images.map(i => <img src={i.url} key={i.description} height="25%" width="25%"/>)}
             <p>{listing.attributes.description}</p>
             <p>Max Guests: {listing.attributes.max_guests}</p>
--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -1,11 +1,53 @@
 
-export default function ListingPage({listing}){
+import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
+export default function ListingPage({listing}){
+    useEffect(() => {
+        getProperty()
+    }, [])
+
+    const [property, setProperty ] = useState('')
+
+    function renderImgs(){
+        if(listing){
+            return listing.attributes.images.map(i => {
+                return (
+                    <>
+                        <img src={i.url} width="25%" height="25%" key={i.description}/>
+                        <p className="caption" key={i.url}>{i.description}</p>
+                    </>
+                )
+            })
+        }
+    }
+
+    async function getProperty(){
+       try {
+        const res  = await fetch(`http://localhost:3001/api/v1/properties/${listing.relationships.property.data.id}`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type" : "application/json "
+            }
+        })
+        if(!res.ok){
+            throw res
+        }
+        const data = await res.json()
+        const property = data.data.attributes
+        setProperty(property)
+       }catch{
+            console.log("Unable to fetch proprerty")
+       }
+    }
 
     return(
         <>
-            <h2>{listing.attributes.title}</h2>
-            <p> {listing.attributes.description}</p>
+            {listing ? <h2>{listing.attributes.title}</h2> : <NavLink to="/listings">See all listings</NavLink>}
+            <h3>{listing.attributes.type_of} in {property.city}</h3>
+            <p> {listing ? listing.attributes.description : <p>Nothing to see here</p>}</p>
+            {renderImgs()}
         </>
     )
 }
