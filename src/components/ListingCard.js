a65d0494fa1107fa5265b592d7693d69cec5c960
joannayhs <joannayuhas@gmail.com>
2021-01-06 11:53:44 -0500

    fixed react warning in listing card

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -5,7 +5,7 @@ export default function ListingCard({listing}){
     return (
         <div className="ListingCard">
             <NavLink to={`listings/${listing.id}`}>{listing.attributes.title}</NavLink><br/>
-            {listing.attributes.images.map(i => <img src={i.url} key={i.description} height="25%" width="25%"/>)}
+            {listing.attributes.images.map(i => <img src={i.url} alt={i.description} key={i.description} height="25%" width="25%"/>)}
             <p>{listing.attributes.description}</p>
             <p>Max Guests: {listing.attributes.max_guests}</p>
             <p>Number of Beds: {listing.attributes.num_of_beds}</p>
