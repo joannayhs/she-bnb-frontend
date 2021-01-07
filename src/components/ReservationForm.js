99c98b911291b5764693ec9eb34d86a4b8a9cd3c
joannayhs <joannayuhas@gmail.com>
2021-01-07 16:36:29 -0500

    added function for reservation form change and submit

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -1,13 +1,32 @@
+import { useState } from 'react'
+import { addReservation } from '../actions/reservations'
+import { connect } from 'react-redux'
 
-export default function ReservationForm({listing}){
+function ReservationForm({listing, addReservation}){
+    const [formData, setFormData] = useState({})
+
+    function handleOnChange(e){
+        const reservationData = formData
+        reservationData[e.target.name] = e.target.value
+        setFormData(reservationData)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        addReservation(formData, listing)
+    }
 
     return(
         <>
         {listing ? <h1>Create Reservation for {listing.attributes.title}</h1> : ''}
-            Start Date: <input type="date" name="start_date" min={Date.now()}/> <br/>
-            End Date: <input type="date" name="end_date"/><br/>
-            Number of Guests: <input type="number" name="num_of_guests" min='1'/><br/>
+        <form onSubmit={handleSubmit}>
+            Start Date: <input type="date" name="start_date" min={Date.now()} onChange={handleOnChange}/> <br/>
+            End Date: <input type="date" name="end_date" onChange={handleOnChange}/><br/>
+            Number of Guests: <input type="number" name="num_of_guests" min='1' onChange={handleOnChange}/><br/>
             <input type="submit" value="Reserve"/>
+        </form>
         </>
     )
 }
+
+export default connect(null, {addReservation})(ReservationForm)
