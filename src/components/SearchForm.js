ebd6832f88b806f22a32059dba43ddf75ce2d6b2
joannayhs <joannayuhas@gmail.com>
2021-02-02 14:52:46 -0500

    started functions for filtering search results

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,26 +1,51 @@
 import { useState } from 'react'
+import { connect } from 'react-redux'
 
-export default function SearchForm(){
-
-    const [formData, setFormData ] = useState('')
+function SearchForm({listings, reservations}){
+    const [formData, setFormData ] = useState({})
+    const [results, setResults ] = useState([])
 
     const handleChange = e => {
+        const searchData = formData 
+        searchData[e.target.name] = e.target.value
+        setFormData(searchData)
+        console.log(formData)
+    }
 
+    function handleSubmit(e){
+        e.preventDefault()
+        getResults(formData)
+    }
+
+    function checkReservations(data){
+
+    }
+    function getResults(data){
+    
     }
 
     return(
         <div className="SearchForm">
-            <form>
-                Enter City
-                <input type="text" placeholder="City" onChange={handleChange}/>
+            <form onSubmit={handleSubmit}>
+                Enter City/State/Zip: 
+                <input type="text" placeholder="City/State/Zip" name="city_state_zip" onChange={handleChange}/>
                 Start Date:
-                <input type="date" onChange={handleChange}/>
+                <input type="date" name="start_date" onChange={handleChange}/>
                 End Date:
-                <input type="date" onChange={handleChange}/>
+                <input type="date" name="end_date" onChange={handleChange}/>
                 Number of Guests:
-                <input type="number" onChange={handleChange}/>
+                <input type="number" name="num_of_guests" onChange={handleChange}/>
                 <input type="submit" value="Search" />
             </form>
         </div>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        listings: state.listings,
+        reservations: state.reservations
+    }
+}
+
+export default connect(mapStateToProps)(SearchForm)
